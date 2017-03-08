fd77b71cc5ae735d662cd657ed3545be474f76a2
Lucas Godshalk <lucas@hertzler.com>
2017-03-08 12:04:10 -0500

    basic repro

---
+++ host/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tmp;
+
+namespace host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HttpHost.Main();
+        }
+    }
+}
